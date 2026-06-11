533d7f4c048129497059c5507063443af6c458e9
rattopio123 <sratto@pioix.edu.ar>
2026-06-11 08:09:13 -0300

    inicio de vs

---
+++ BACK/index.js
@@ -0,0 +1,28 @@
+var express = require('express'); //Tipo de servidor: Express
+var bodyParser = require('body-parser'); //Convierte los JSON
+var cors = require('cors');
+const { realizarQuery } = require('./modulos/mysql');
+
+var app = express(); //Inicializo express
+var port = process.env.PORT || 4000; //Ejecuto el servidor en el puerto 4000
+
+// Convierte una petición recibida (POST-GET...) a objeto JSON
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(cors());
+
+//Pongo el servidor a escuchar
+app.listen(port, function(){
+    console.log(`Server running in http://localhost:${port}`);
+});
+
+app.get('/', function(req, res){
+    res.status(200).send({
+        message: 'GET Home route working fine!'
+    });
+});
+
+/**
+ * req = request. en este objeto voy a tener todo lo que reciba del cliente
+ * res = response. Voy a responderle al cliente
+*/ 
